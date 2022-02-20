dad596d9a89498dcd7847d43a7c6c704191dccf1
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-20 01:46:32 -0300

    test: FileListUsers Should return a list of Users on success

--- test/data/usecases/file.list.users.spec.ts
+++ test/data/usecases/file.list.users.spec.ts
@@ -1,13 +1,34 @@
 import { FileListUsers } from '@/data/usecases';
 import { ListUsersRepositorySpy } from '../mocks';
 
+type SutTypes = {
+  sut: FileListUsers;
+  listUsersRepositorySpy: ListUsersRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const listUsersRepositorySpy = new ListUsersRepositorySpy();
+  const sut = new FileListUsers(listUsersRepositorySpy);
+  return {
+    sut,
+    listUsersRepositorySpy,
+  };
+};
+
 describe('FileListUsers', () => {
-  it('Should call ListUsersRepository', async () => {
-    const listUsersRepositorySpy = new ListUsersRepositorySpy();
-    const sut = new FileListUsers(listUsersRepositorySpy);
+  test('Should call ListUsersRepository', async () => {
+    const { sut, listUsersRepositorySpy } = makeSut();
 
     await sut.listUsers();
 
     expect(listUsersRepositorySpy.callCount).toBe(1);
+  });
+
+  test('Should return a list of Users on success', async () => {
+    const { sut, listUsersRepositorySpy } = makeSut();
+
+    const output = await sut.listUsers();
+
+    expect(output).toBe(listUsersRepositorySpy.output);
   });
 });
