3da9269286e1424defa3687206a43af55567dc0b
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-20 20:20:01 -0300

    test: ListUsersFileRepository Should list all users on success

--- test/infra/file/users/list.users.file.repository.spec.ts
+++ test/infra/file/users/list.users.file.repository.spec.ts
@@ -2,21 +2,54 @@ import { ListUsersFileRepository } from '@/infra/file';
 import { promises as fs } from 'fs';
 import faker from '@faker-js/faker';
 import * as path from 'path';
+import { mockListUsersOutput } from 'test/domain/mocks';
 
 const mockUsersFileEnv = () => {
   return path.join(__dirname, faker.system.fileName());
 };
 
+const makeSut = () => {
+  return new ListUsersFileRepository();
+};
+
 describe('ListUsersFileRepository', () => {
-  test('Should list an empty list', async () => {
-    const mockedUsersFile = mockUsersFileEnv();
+  const backupUsersFile = process.env.USERS_FILE;
+  const mockedUsersFile = mockUsersFileEnv();
+
+  beforeAll(async () => {
     process.env.USERS_FILE = mockedUsersFile;
+  });
+
+  afterAll(async () => {
+    process.env.USERS_FILE = backupUsersFile;
+  });
+
+  beforeEach(async () => {
+    await fs.open(mockedUsersFile, 'w');
+  });
+
+  afterEach(async () => {
+    await fs.unlink(mockedUsersFile);
+  });
+
+  test('Should list an empty list', async () => {
     await fs.writeFile(mockedUsersFile, JSON.stringify([]));
-    const sut = new ListUsersFileRepository();
+    const sut = makeSut();
 
     const users = await sut.listUsers();
 
     expect(users.length).toBe(0);
-    await fs.unlink(mockedUsersFile);
+  });
+
+  test('Should list all users on success', async () => {
+    const userModels = mockListUsersOutput();
+    await fs.writeFile(mockedUsersFile, JSON.stringify(userModels));
+    const sut = makeSut();
+
+    const users = await sut.listUsers();
+
+    expect(users.length).toBe(2);
+    expect(users[0]).toStrictEqual(userModels[0]);
+    expect(users[1]).toStrictEqual(userModels[1]);
   });
 });
