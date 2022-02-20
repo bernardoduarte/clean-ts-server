ea77d11224cf787cdb72a085a45b38b6ff8dc068
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-20 19:58:41 -0300

    test: ListUsersFileRepository Should list an empty list passing without manual environment variable setup

--- test/infra/file/users/list.users.file.repository.spec.ts
+++ test/infra/file/users/list.users.file.repository.spec.ts
@@ -1,14 +1,22 @@
 import { ListUsersFileRepository } from '@/infra/file';
 import { promises as fs } from 'fs';
+import faker from '@faker-js/faker';
+import * as path from 'path';
+
+const mockUsersFileEnv = () => {
+  return path.join(__dirname, faker.system.fileName());
+};
 
 describe('ListUsersFileRepository', () => {
   test('Should list an empty list', async () => {
-    await fs.writeFile(process.env.USERS_FILE, JSON.stringify([]));
+    const mockedUsersFile = mockUsersFileEnv();
+    process.env.USERS_FILE = mockedUsersFile;
+    await fs.writeFile(mockedUsersFile, JSON.stringify([]));
     const sut = new ListUsersFileRepository();
 
     const users = await sut.listUsers();
 
     expect(users.length).toBe(0);
-    await fs.unlink(process.env.USERS_FILE);
+    await fs.unlink(mockedUsersFile);
   });
 });
