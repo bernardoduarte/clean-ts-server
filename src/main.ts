1d56cd7d7281d63efbd43af9c1c80f39a8926afb
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-21 14:21:02 -0300

    refactor: renamed app module to main module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MainModule } from './main.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const nest = await NestFactory.create(MainModule);
+  await nest.listen(3000);
 }
 bootstrap();
