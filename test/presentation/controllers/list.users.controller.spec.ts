8e160f6dca97e13e0cda9666f7bb33ffd5475658
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-20 22:59:25 -0300

    test: ListUsersController Should return 200 on success

--- test/presentation/controllers/list.users.controller.spec.ts
+++ test/presentation/controllers/list.users.controller.spec.ts
@@ -1,13 +1,33 @@
 import { ListUsersController } from '@/presentation/controllers';
+import { ok } from '@/presentation/helpers';
 import { ListUsersSpy } from '../mocks';
+
+type SutTypes = {
+  sut: ListUsersController;
+  listUsersSpy: ListUsersSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const listUsersSpy = new ListUsersSpy();
+  const sut = new ListUsersController(listUsersSpy);
+  return {
+    sut,
+    listUsersSpy,
+  };
+};
 
 describe('ListUsersController', () => {
   test('Should call ListUsers', async () => {
-    const listUsersSpy = new ListUsersSpy();
-    const sut = new ListUsersController(listUsersSpy);
+    const { sut, listUsersSpy } = makeSut();
 
     await sut.handle();
 
     expect(listUsersSpy.callCount).toBe(1);
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut, listUsersSpy } = makeSut();
+    const httpResponse = await sut.handle();
+    expect(httpResponse).toEqual(ok(listUsersSpy.output));
   });
 });
