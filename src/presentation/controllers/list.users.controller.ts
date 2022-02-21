b77683ef904f2016b6752186d60f88edc0ccb2ba
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-21 11:14:51 -0300

    feat: ListUsersController returns server error if an error is thrown

--- src/presentation/controllers/list.users.controller.ts
+++ src/presentation/controllers/list.users.controller.ts
@@ -1,12 +1,16 @@
 import { ListUsers } from '@/domain/usecases';
-import { noContent, ok } from '../helpers';
+import { noContent, ok, serverError } from '../helpers';
 import { Controller, HttpResponse } from '../protocols';
 
 export class ListUsersController implements Controller {
   constructor(private readonly listUsersFileRepository: ListUsers) {}
 
   async handle(): Promise<HttpResponse> {
-    const users = await this.listUsersFileRepository.listUsers();
-    return users.length ? ok(users) : noContent();
+    try {
+      const users = await this.listUsersFileRepository.listUsers();
+      return users.length ? ok(users) : noContent();
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
