09171f3bcbaa13aa7ae6c81fc6a77759056759ae
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-20 22:36:41 -0300

    feat: implemented http ok helper

--- src/presentation/helpers/http.helper.ts
+++ src/presentation/helpers/http.helper.ts
@@ -5,3 +5,8 @@ export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack),
 });
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+});
