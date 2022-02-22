e94159bc2fe4d03a79ac0cffe08d10437fd01880
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-21 22:43:18 -0300

    refactor: replaced fs usage with FileHelper

--- src/infra/file/users/list.users.file.repository.ts
+++ src/infra/file/users/list.users.file.repository.ts
@@ -1,11 +1,8 @@
 import { ListUsersRepository } from '@/data/protocols';
-
-import { promises as fs } from 'fs';
+import { FileHelper } from './file-helper';
 
 export class ListUsersFileRepository implements ListUsersRepository {
   async listUsers(): Promise<ListUsersRepository.Output> {
-    const buffer = await fs.readFile(process.env.USERS_FILE);
-    const users = JSON.parse(buffer.toString());
-    return users;
+    return FileHelper.readJsonFile(process.env.USERS_FILE);
   }
 }
