d01d6d1c31359fde187ae9b8aff1f2ed1d938a59
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2022-02-21 23:07:15 -0300

    test: User routes GET /users Should return 200 on list users

---
+++ test/main/routes/user.routes.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { MainModule } from '@/main/module';
+import { getWritableFilePath } from 'test/infra/mocks';
+import { FileHelper } from '@/infra/file/users/file-helper';
+import { mockListUsersOutput } from 'test/domain/mocks';
+
+describe('User routes', () => {
+  let app: INestApplication;
+  let mockedUsersFile: string;
+
+  beforeEach(async () => {
+    mockedUsersFile = getWritableFilePath();
+    process.env.USERS_FILE = mockedUsersFile;
+    await FileHelper.createFile(mockedUsersFile);
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [MainModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    try {
+      await FileHelper.deleteFile(mockedUsersFile);
+    } catch (error) {}
+  });
+
+  describe('GET /users', () => {
+    test('Should return 200 on list users', async () => {
+      await FileHelper.writeJsonFile(mockedUsersFile, mockListUsersOutput());
+      await request(app.getHttpServer()).get('/users').expect(200);
+    });
+  });
+});
